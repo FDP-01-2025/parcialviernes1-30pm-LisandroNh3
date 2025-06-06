7d9c8ee5f19027cf3fece9c4b68140ad9b3fa9ff
LisandroNh3 <00025825@uca.edu.sv>
2025-06-06 14:31:40 -0600

    Agregando primer ejercicio

--- carrito.cpp
+++ carrito.cpp
@@ -10,5 +10,29 @@ int main()
     cout << "1- Ver productos disponibles" << endl;
     cout << "2- Agregar productos al carro" << endl;
     cout << "3- Ver el carrito de compras" << endl;
-    cout << "4- Finalizaer compra y salir " << endl;
+    cout << "4- Finalizar compra y salir " << endl;
+    cin >> eleccion;
+
+    switch (eleccion)
+    {
+    case 1:
+        cout << "Nuestros productos disponibles son :" << endl;
+         cout << "1- Audifonos inalambricos $75" << endl;
+         cout << "2- Laptop basica $385" << endl;
+         cout << "3- Tablet basica $285" << endl;
+         cout << "Desea agregar algo de aqui" << endl;
+         cin >> compra;
+         return 0;
+        break;
+
+        case 2:
+
+        cout << "De los articulos seleccionados digite cual desea agregar al carrito" << endl;
+
+    
+    default:
+        break;
+    }
+
+
 }
